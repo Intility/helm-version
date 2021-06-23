6e0b7f66d49fd063b964df62332e1e63366f9106
Herman Jensen <herman.jensen@intility.no>
2021-06-23 18:57:39 +0200

    add optional prefix, default value "v"

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ import YAML from 'yaml';
 import fs from 'fs';
 import { readPackageUpSync } from 'read-pkg-up';
 
-if (process.argv.length === 3) {
-  const [,,chartPath] = process.argv;
+if (process.argv.length > 2) {
+  const [,, chartPath, prefix = 'v' ] = process.argv;
   
   const helmChart = YAML.parseDocument(fs.readFileSync(`${chartPath}/Chart.yaml`, 'utf8'));
 
   const version = readPackageUpSync().packageJson.version;
   
-  helmChart.set("appVersion", version);
+  helmChart.set('appVersion', prefix + version);
   
   fs.writeFileSync(`${chartPath}/Chart.yaml`, helmChart.toString());
 } else {
   console.log('Usage:');
-  console.log('helm-version <chart>\n');
+  console.log('helm-version <chart> <prefix = "v">\n');
   process.exit(0);
 }
