b5b31be01f6c2f31aa082506e12e6bc3dcf0d764
marek <pl.palczynski@gmail.com>
2022-08-09 14:35:03 +0200

    Add sync flag synchronizing helm chart version and app version

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ import fs from 'fs';
 import { readPackageUpSync } from 'read-pkg-up';
 
 if (process.argv.length > 2) {
-  const [,, chartPath, prefix = 'v' ] = process.argv;
-  
+  const [,, chartPath, prefix = 'v', sync = 'true'] = process.argv;
+
   const helmChart = YAML.parseDocument(fs.readFileSync(`${chartPath}/Chart.yaml`, 'utf8'));
 
   const version = readPackageUpSync().packageJson.version;
   
   helmChart.set('appVersion', prefix + version);
+  if(sync === 'true') {
+    helmChart.set('version', prefix + version);
+  }
   
   fs.writeFileSync(`${chartPath}/Chart.yaml`, helmChart.toString());
 } else {
   console.log('Usage:');
-  console.log('helm-version <chart> <prefix = "v">\n');
+  console.log('helm-version <chart> <prefix = "v"> <sync = "true">\n');
   process.exit(0);
 }
