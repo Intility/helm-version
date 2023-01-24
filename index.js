6c845808bc455b131ad34e5ad08e8d84f97ccc65
Herman Jensen <herman.jensen@intility.no>
2023-01-24 16:09:35 +0100

    feat: bump helm version (#3)
    * feat: bump helm version
    
    * docs: add comment to env usage

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 
 import YAML from 'yaml';
 import fs from 'fs';
-import { readPackageUpSync } from 'read-pkg-up';
+import semver from 'semver'
 
 if (process.argv.length > 2) {
   const [,, chartPath, prefix = 'v' ] = process.argv;
   
   const helmChart = YAML.parseDocument(fs.readFileSync(`${chartPath}/Chart.yaml`, 'utf8'));
 
-  const version = readPackageUpSync().packageJson.version;
-  
-  helmChart.set('appVersion', prefix + version);
-  
+  // get app versions and calculate diff
+  const prevAppVersion = helmChart.get('appVersion');
+
+  // https://docs.npmjs.com/cli/v9/using-npm/scripts?v=true#packagejson-vars
+  const newAppVersion = process.env.npm_package_version;
+  const appVersionDiff = semver.diff(prevAppVersion, newAppVersion);
+
+  // get helm version and apply the app version diff
+  const prevHelmVersion = helmChart.get('version');
+  const newHelmVersion = semver.inc(prevHelmVersion, appVersionDiff)
+
+  // apply the new versions
+  helmChart.set('appVersion', prefix + newAppVersion);
+  helmChart.set('version', newHelmVersion);
+
   fs.writeFileSync(`${chartPath}/Chart.yaml`, helmChart.toString());
 } else {
   console.log('Usage:');
